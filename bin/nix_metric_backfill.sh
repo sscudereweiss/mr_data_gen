00c9f036d7f5308a3e31521459f10d0e98f10968
sscudereweiss <sscudereweiss@splunk.com>
2023-12-12 19:28:33 -0500

    updated to shuf

--- bin/nix_metric_backfill.sh
+++ bin/nix_metric_backfill.sh
@@ -7,13 +7,13 @@ if [ -r "$file" ]; then
           echo $line
           for ((i=1; i<=$minutes_backfill; i++)); do
                back_time=$(date -d "$backfill_start -"$i" minutes" +%s)
-               CPU_Idle=$(( (RANDOM % 30) + 10 )) # Random CPU usage between 10% and 90%
-               Memory_Usage=$(( (RANDOM % 70) + 30 )) # Random Memory usage between 30% and 100%
-               Disk_Free=$(( (RANDOM % 60) + 20 )) # Random Disk free between 20% and 80%
-               Disk_Read=$(( (RANDOM % 100) + 2000 )) # Random Disk read between 100 and 2100 bytes
-               Disk_Write=$(( (RANDOM % 100) + 2000 )) # Random Disk write between 100 and 2100 bytes
-               Network_Bytes_Received=$(( (RANDOM % 100) + 1000000 )) # Random Bytes received/sec between 1MB and 11MB
-               Network_Bytes_Sent=$(( (RANDOM % 5000000) + 1000000 )) # Random Bytes sent/sec between 1MB and 6MB
+               CPU_Idle=$(shuf -i 30-80 -n 1) # Random CPU usage between 10% and 80%
+               Memory_Usage=$(shuf -i 60-80 -n 1) # Random Memory usage between 30% and 80%
+               Disk_Free=$(shuf -i 40-60 -n 1) # Random Disk free between 40% and 80%
+               Disk_Read=$(shuf -i 100-2100 -n 1) # Random Disk read between 100 and 2100 bytes
+               Disk_Write=$(shuf -i 100-2100 -n 1) # Random Disk write between 100 and 2100 bytes
+               Network_Bytes_Received=$(shuf -i 100-1000000 -n 1 # Random Bytes received/sec between 1MB and 11MB
+               Network_Bytes_Sent=$(shuf -i 100-600000 -n 1) # Random Bytes sent/sec between 1MB and 6MB
                curl -k -s -o /dev/null https://localhost:8088/services/collector -H "Authorization: Splunk e675db8b-7149-48ed-9483-4f3d0b070f7e" -d '{ "time": "'${back_time}'", "event": "metric", "source": "mcollect", "sourcetype": "mcollect_stash", "host": "'${line}'", "fields": { "metric_name:cpu.idle": "'${CPU_Idle}'", "metric_name:memory.used": "'${Memory_Usage}'", "metric_name:df.used": "'${Disk_Free}'", "metric_name:disk.ops.read": "'${Disk_Read}'", "metric_name:disk.ops.write": "'${Disk_Write}'", "metric_name:interface.octets.rx": "'${Network_Bytes_Received}'", "metric_name:interface.octets.tx": "'${Network_Bytes_Sent}'"}}' &
           done
      done < "$file"
--- bin/windows_metric_backfill.sh
+++ bin/windows_metric_backfill.sh
@@ -7,13 +7,13 @@ if [ -r "$file" ]; then
           echo $line
           for ((i=1; i<=$minutes_backfill; i++)); do
                back_time=$(date -d "$backfill_start -"$i" minutes" +%s)
-               Processor_Idle_Time=$((40 + RANDOM % 10))
-               Memory_Committed_Bytes_In_Use=$((60 + RANDOM % 10 ))
-               LogicalDisk_Free_Space=$((50 + RANDOM %10 ))
-               Avg_Disk_Bytes_Read=$((1800 + RANDOM % 100 ))
-               Avg_Disk_Bytes_Write=$((1800 + RANDOM % 100 ))
-               Network_Interface_Bytes_Received_sec=$((7000000 + RANDOM % 100))
-               Network_Interface_Bytes_Sent_sec=$((2000000 + RANDOM % 100))
+               Processor_Idle_Time=$(shuf -i 30-80 -n 1)
+               Memory_Committed_Bytes_In_Use=$(shuf -i 60-80 -n 1)
+               LogicalDisk_Free_Space=$(shuf -i 40-60 -n 1)
+               Avg_Disk_Bytes_Read=$(shuf -i 40000-60000 -n 1)
+               Avg_Disk_Bytes_Write=$(shuf -i 40000-60000 -n 1)
+               Network_Interface_Bytes_Received_sec=$(shuf -i 60000-70000 -n 1)
+               Network_Interface_Bytes_Sent_sec=$(shuf -i 20000-40000 -n 1)
                curl -k -s -o /dev/null https://localhost:8088/services/collector -H "Authorization: Splunk e675db8b-7149-48ed-9483-4f3d0b070f7e" -d '{ "time": "'${back_time}'", "event": "metric", "source": "mcollect", "sourcetype": "mcollect_stash", "host": "'${line}'", "fields": { "metric_name:Avg._Disk_Bytes/Read": "'${Avg_Disk_Bytes_Read}'", "metric_name:Avg._Disk_Bytes/Write": "'${Avg_Disk_Bytes_Write}'", "metric_name:LogicalDisk.%_Free_Space": "'${LogicalDisk_Free_Space}'", "metric_name:Memory.%_Committed_Bytes_In_Use": "'${Memory_Committed_Bytes_In_Use}'", "metric_name:Network_Interface.Bytes_Received/sec": "'${Network_Interface_Bytes_Received_sec}'", "metric_name:Network_Interface.Bytes_Sent/sec": "'${Network_Interface_Bytes_Sent_sec}'", "metric_name:Processor.%_Idle_Time": "'${Processor_Idle_Time}'"}}' &
                done
      done < "$file"
