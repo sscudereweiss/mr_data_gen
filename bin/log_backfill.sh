c988c8fcf11e1a8fb6be397a6012ed2140935922
sscudereweiss <sscudereweiss@splunk.com>
2023-12-18 16:44:35 -0500

    time

--- bin/log_backfill.sh
+++ bin/log_backfill.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 start_time=`date +%s.%N`
 #minutes_backfill=10080
-minutes_backfill=1440
+minutes_backfill=2880
 #minutes_backfill=10
 hec_token="60469025-55a6-4718-81af-4c047f13401b"
 file="/opt/splunk/etc/apps/mr_data_gen/bin/db_entity_list.txt"
@@ -10,7 +10,7 @@ backfill_start=$(date -u)
 if [ -r "$file" ]; then
      while IFS= read -r line; do
           echo $line
-          (for ((i=5; i<=$minutes_backfill; i+=5)); do
+          (for ((i=1440; i<=$minutes_backfill; i+=5)); do
                #back_time=$(date -d "$backfill_start -"$i" minutes" +"%Y/%m/%d %H:%M:%S")
                back_time=$(date -d "$backfill_start -"$i" minutes" +%s)
                log_line="[CRITICAL] /opt/mysql/bin/mysqld: Disk is full writing '/mysqllog/binlog/localhost-3306-bin.000020' (Errcode: 28). Waiting for someone to free space... Retry in 60 secs"
