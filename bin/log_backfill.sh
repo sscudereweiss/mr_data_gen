5661fc8b4424d5f6a0fee8ea8f30111fce9217b9
sscudereweiss <sscudereweiss@splunk.com>
2023-12-18 16:10:02 -0500

    changed to one week

--- bin/log_backfill.sh
+++ bin/log_backfill.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 start_time=`date +%s.%N`
-#minutes_backfill=10080
+minutes_backfill=10080
 #minutes_backfill=1440
-minutes_backfill=10
+#minutes_backfill=10
 hec_token="60469025-55a6-4718-81af-4c047f13401b"
 file="/opt/splunk/etc/apps/mr_data_gen/bin/db_entity_list.txt"
 #file="db_entity_list.txt"
@@ -14,7 +14,7 @@ if [ -r "$file" ]; then
                #back_time=$(date -d "$backfill_start -"$i" minutes" +"%Y/%m/%d %H:%M:%S")
                back_time=$(date -d "$backfill_start -"$i" minutes" +%s)
                log_line="[CRITICAL] /opt/mysql/bin/mysqld: Disk is full writing '/mysqllog/binlog/localhost-3306-bin.000020' (Errcode: 28). Waiting for someone to free space... Retry in 60 secs"
-               curl -k https://localhost:8088/services/collector -H "Authorization: Splunk ${hec_token}" -d '{"time": "'${back_time}'", "event": "[CRITICAL] /opt/mysql/bin/mysqld: Disk is full writing '/mysqllog/binlog/localhost-3306-bin.000020' (Errcode: 28). Waiting for someone to free space... Retry in 60 secs"}'
+               curl -k -s -o /dev/null https://localhost:8088/services/collector -H "Authorization: Splunk ${hec_token}" -d '{"time": "'${back_time}'", "event": "[CRITICAL] /opt/mysql/bin/mysqld: Disk is full writing '/mysqllog/binlog/localhost-3306-bin.000020' (Errcode: 28). Waiting for someone to free space... Retry in 60 secs"}'
                done) &
      done < "$file"
      wait
