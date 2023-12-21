ad2d44d27b1d2dcae0ca5718ce8a05aea5dce3fe
sscudereweiss <sscudereweiss@splunk.com>
2023-12-21 09:35:26 -0500

    added progress bars

--- bin/nix_metric_backfill.sh
+++ bin/nix_metric_backfill.sh
@@ -4,6 +4,23 @@ minutes_backfill=10080
 #minutes_backfill=1440
 file="/opt/splunk/etc/apps/mr_data_gen/bin/nix_entity_list.txt"
 backfill_start=$(date -u)
+
+function ProgressBar {
+# Process data
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+# Build progressbar string lengths
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+
+# 1.2 Build progressbar strings and print the ProgressBar line
+# 1.2.1 Output example:                           
+# 1.2.1.1 Progress : [########################################] 100%
+printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
+
+}
+
 if [ -r "$file" ]; then
      while IFS= read -r line; do
           echo $line
@@ -16,7 +33,8 @@ if [ -r "$file" ]; then
                Disk_Write=$(shuf -i 100-2100 -n 1) # Random Disk write between 100 and 2100 bytes
                Network_Bytes_Received=$(shuf -i 800000-1000000 -n 1) # Random Bytes received/sec between 1MB and 11MB
                Network_Bytes_Sent=$(shuf -i 2000000-3000000 -n 1) # Random Bytes sent/sec between 1MB and 6MB
-               curl -k -s -o /dev/null https://localhost:8088/services/collector -H "Authorization: Splunk e675db8b-7149-48ed-9483-4f3d0b070f7e" -d '{ "time": "'${back_time}'", "event": "metric", "source": "mcollect", "sourcetype": "mcollect_stash", "host": "'${line}'", "fields": { "metric_name:cpu.idle": "'${CPU_Idle}'", "metric_name:memory.used": "'${Memory_Usage}'", "metric_name:df.used": "'${Disk_Free}'", "metric_name:disk.ops.read": "'${Disk_Read}'", "metric_name:disk.ops.write": "'${Disk_Write}'", "metric_name:interface.octets.rx": "'${Network_Bytes_Received}'", "metric_name:interface.octets.tx": "'${Network_Bytes_Sent}'"}}' &
+               (curl -k -s -o /dev/null https://localhost:8088/services/collector -H "Authorization: Splunk e675db8b-7149-48ed-9483-4f3d0b070f7e" -d '{ "time": "'${back_time}'", "event": "metric", "source": "mcollect", "sourcetype": "mcollect_stash", "host": "'${line}'", "fields": { "metric_name:cpu.idle": "'${CPU_Idle}'", "metric_name:memory.used": "'${Memory_Usage}'", "metric_name:df.used": "'${Disk_Free}'", "metric_name:disk.ops.read": "'${Disk_Read}'", "metric_name:disk.ops.write": "'${Disk_Write}'", "metric_name:interface.octets.rx": "'${Network_Bytes_Received}'", "metric_name:interface.octets.tx": "'${Network_Bytes_Sent}'"}}') &
+               ProgressBar ${i} ${minutes_backfill}
           done ) &
      done < "$file"
      wait
--- bin/windows_metric_backfill.sh
+++ bin/windows_metric_backfill.sh
@@ -1,9 +1,25 @@
 #! /bin/bash
-start_time=`date +%s.%N`
 minutes_backfill=10080
 #minutes_backfill=1440
 file="/opt/splunk/etc/apps/mr_data_gen/bin/windows_entity_list.txt"
 backfill_start=$(date -u)
+
+function ProgressBar {
+# Process data
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+# Build progressbar string lengths
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+
+# 1.2 Build progressbar strings and print the ProgressBar line
+# 1.2.1 Output example:                           
+# 1.2.1.1 Progress : [########################################] 100%
+printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
+
+}
+
 if [ -r "$file" ]; then
      while IFS= read -r line; do
           echo $line
@@ -16,14 +32,13 @@ if [ -r "$file" ]; then
                Avg_Disk_Bytes_Write=$(shuf -i 40000-60000 -n 1)
                Network_Interface_Bytes_Received_sec=$(shuf -i 800000-1000000 -n 1)
                Network_Interface_Bytes_Sent_sec=$(shuf -i 2000000-3000000 -n 1)
-               curl -k -s -o /dev/null https://localhost:8088/services/collector -H "Authorization: Splunk e675db8b-7149-48ed-9483-4f3d0b070f7e" -d '{ "time": "'${back_time}'", "event": "metric", "source": "mcollect", "sourcetype": "mcollect_stash", "host": "'${line}'", "fields": { "metric_name:Avg._Disk_Bytes/Read": "'${Avg_Disk_Bytes_Read}'", "metric_name:Avg._Disk_Bytes/Write": "'${Avg_Disk_Bytes_Write}'", "metric_name:LogicalDisk.%_Free_Space": "'${LogicalDisk_Free_Space}'", "metric_name:Memory.%_Committed_Bytes_In_Use": "'${Memory_Committed_Bytes_In_Use}'", "metric_name:Network_Interface.Bytes_Received/sec": "'${Network_Interface_Bytes_Received_sec}'", "metric_name:Network_Interface.Bytes_Sent/sec": "'${Network_Interface_Bytes_Sent_sec}'", "metric_name:Processor.%_Idle_Time": "'${Processor_Idle_Time}'"}}'
-               done ) &
+               (curl -k -s -o /dev/null https://localhost:8088/services/collector -H "Authorization: Splunk e675db8b-7149-48ed-9483-4f3d0b070f7e" -d '{ "time": "'${back_time}'", "event": "metric", "source": "mcollect", "sourcetype": "mcollect_stash", "host": "'${line}'", "fields": { "metric_name:Avg._Disk_Bytes/Read": "'${Avg_Disk_Bytes_Read}'", "metric_name:Avg._Disk_Bytes/Write": "'${Avg_Disk_Bytes_Write}'", "metric_name:LogicalDisk.%_Free_Space": "'${LogicalDisk_Free_Space}'", "metric_name:Memory.%_Committed_Bytes_In_Use": "'${Memory_Committed_Bytes_In_Use}'", "metric_name:Network_Interface.Bytes_Received/sec": "'${Network_Interface_Bytes_Received_sec}'", "metric_name:Network_Interface.Bytes_Sent/sec": "'${Network_Interface_Bytes_Sent_sec}'", "metric_name:Processor.%_Idle_Time": "'${Processor_Idle_Time}'"}}') &
+               ProgressBar ${i} ${minutes_backfill}
+          done ) &
      done < "$file"
      wait
 else
     echo "File $file does not exist or is not readable."
 fi
 
-end_time=`date +%s.%N`
-runtime=$( echo "$end_time - $start_time" | bc -l )
-echo "windows backfill finished in $runtime seconds"
+echo "windows backfill finished in $SECONDS seconds"
