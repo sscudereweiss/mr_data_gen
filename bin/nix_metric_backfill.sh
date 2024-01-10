f40e39538afbb40080531df9be66019362ab8a2c
sscudereweiss <sscudereweiss@splunk.com>
2024-01-10 15:02:49 -0500

    up

--- bin/nix_metric_backfill.sh
+++ bin/nix_metric_backfill.sh
@@ -30,7 +30,7 @@ if [ -r "$file" ]; then
                 Network_Bytes_Received=$(shuf -i 900000-1000000 -n 1) # Random Bytes received/sec between 1MB and 11MB
                 Network_Bytes_Sent=$(shuf -i 2500000-3000000 -n 1) # Random Bytes sent/sec between 1MB and 6MB
             fi
-            curl -k -s -o /dev/null https://localhost:8088/services/collector -H "Authorization: Splunk e675db8b-7149-48ed-9483-4f3d0b070f7e" -d '{ "time": "'${back_time}'", "event": "metric", "source": "mcollect", "sourcetype": "mcollect_stash", "host": "'${line}'", "fields": { "metric_name:cpu.idle": "'${CPU_Idle}'", "metric_name:memory.used": "'${Memory_Usage}'", "metric_name:df.used": "'${Disk_Free}'", "metric_name:disk.ops.read": "'${Disk_Read}'", "metric_name:disk.ops.write": "'${Disk_Write}'", "metric_name:interface.octets.rx": "'${Network_Bytes_Received}'", "metric_name:interface.octets.tx": "'${Network_Bytes_Sent}'"}}'
+            curl -k -s -o /dev/null https://localhost:8088/services/collector -H "Authorization: Splunk e675db8b-7149-48ed-9483-4f3d0b070f7e" -d '{ "time": "'${back_time}'", "event": "metric", "source": "'${line}'", "sourcetype": "mcollect_stash", "host": "'${line}'", "fields": { "metric_name:cpu.idle": "'${CPU_Idle}'", "metric_name:memory.used": "'${Memory_Usage}'", "metric_name:df.used": "'${Disk_Free}'", "metric_name:disk.ops.read": "'${Disk_Read}'", "metric_name:disk.ops.write": "'${Disk_Write}'", "metric_name:interface.octets.rx": "'${Network_Bytes_Received}'", "metric_name:interface.octets.tx": "'${Network_Bytes_Sent}'"}}'
           done
           echo "Backfill Complete for : ${line}") &
      done < "$file"
