bb16a8968623518d2a10bc7aec1f64bad9975941
sscudereweiss <sscudereweiss@splunk.com>
2024-03-01 15:05:24 -0500

    added background task

--- bin/summary_metric_backfill.sh
+++ bin/summary_metric_backfill.sh
@@ -13,7 +13,7 @@ alert_level=2
 alert_severity=normal
 color=\#99D18B
 
-for ((i=1; i<=$minutes_backfill; i++)); do
+(for ((i=1; i<=$minutes_backfill; i++)); do
     back_time=$(date -d "$backfill_start -"$i" minutes" +%s)
     info_min_time=$(date -d "$backfill_start -"$(($i+1))" minutes" +%s)
     
@@ -22,5 +22,7 @@ for ((i=1; i<=$minutes_backfill; i++)); do
     curl -k https://localhost:8088/services/collector -H "Authorization: Splunk ${hec_token}" -d '{"time":"'${back_time}'","event":"metric", "index": "itsi_summary_metrics", "source":"service_health_metrics_monitor","sourcetype":"itsi_summary:metrics","fields":{"alert_period":"1","source":"service_health_metrics_monitor","sourcetype":"stash","info_max_time":"'${back_time}'","info_min_time":"'${info_min_time}'","info_search_time":"'${back_time}'","is_null_alert_value":"0","itsi_kpi_id":"'${kpi_id}'","itsi_service_id":"'${service_id}'","itsi_team_id":"default_itsi_security_group","scoretype":"service_health","metric_name:alert_level":"'${alert_level}'"}}'
     
     curl -k -s -o /dev/null https://localhost:8088/services/collector -H "Authorization: Splunk ${hec_token}" -d '{"time": "'${back_time}'", "index": "itsi_summary", "sourcetype": "stash", "event": "search_name=service_health_monitor, search_now='${back_time}', info_min_time='${info_min_time}', info_max_time='${back_time}', info_search_time='${back_time}', kpi=ServiceHealthScore, color="'${color}'", kpiid="'${kpi_id}'", scoretype=service_health, serviceid="'${service_id}'", itsi_service_id="'${service_id}'", alert_level='${alert_level}', itsi_kpi_id="'${kpi_id}'", alert_severity='${alert_severity}', health_score="'${service_health_score}'", severity_label='${alert_severity}', severity_value="'${service_health_score}'""}'
-done
+done) &
+
+wait
 
